1516432eaf5bf4ec40bb2cb0db82401bba32db09
Jarno Luoma-Nirva <jln@iki.fi>
2018-07-24 14:36:36 +0300

    Add inline CSS to button

--- src/App.js
+++ src/App.js
@@ -30,10 +30,19 @@ class App extends Component {
     });
   }
   render() {
+    const style = {
+      backgroundColor: 'white',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1> Moro </h1>
-        <button onClick={this.switchNameHandler.bind(this, 'Huck')}>Switch name</button>
+        <button
+          style={style}
+          onClick={this.switchNameHandler.bind(this, 'Huck')}>Switch name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
