b73b486929a1c89c8ef845e9469be9afae2eaa64
Jarno Luoma-Nirva <jln@iki.fi>
2018-07-24 12:11:05 +0300

    Add Person component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Moro </h1>
+        <Person name="Hux" age="35">I like hats</Person>
+        <Person name="Poe" age="40"/>
       </div>
     );
     // Same as:
