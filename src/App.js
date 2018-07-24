67e4099eebd8bcf55b9ee07d5723432a6d1b84ac
Jarno Luoma-Nirva <jln@iki.fi>
2018-07-24 12:54:03 +0300

    Save person data in state object

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Hux', age: 35 },
+      { name: 'Poe', age: 30 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Moro </h1>
-        <Person name="Hux" age="35">I like hats</Person>
-        <Person name="Poe" age="40"/>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>I like hats</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I like racing</Person>
       </div>
     );
     // Same as:
