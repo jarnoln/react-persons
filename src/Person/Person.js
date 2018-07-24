80a044ba18a2403d7c493e757be65d6bf3ca6d61
Jarno Luoma-Nirva <jln@iki.fi>
2018-07-24 14:23:33 +0300

    Add CSS styling to Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
+        <div className="person">
             <p>I'm {props.name}, {props.age} years old</p>
             <p> {props.children} </p>
-            <input type="text" onChange={props.changed} />
+            <input type="text" onChange={props.changed} defaultValue={props.name} />
         </div>
     )
 }
